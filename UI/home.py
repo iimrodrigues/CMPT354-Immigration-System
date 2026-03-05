94aa9fd31d4fa2bcf1dd6b613006c8efa0a99847
iim <iim@sfu.ca>
2026-03-05 11:54:42 -0800

    fix: UI folder + add: example UI

---
+++ UI/home.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Immigration System")
+root.geometry("400x300")
+
+label = tk.Label(root, text="Welcome", font=("Arial", 24))
+label.pack()
+
+root.mainloop()
