08e7d0922eda6ca0c46a60ffeec2c0478f996bee
angelhmu <hershey3614@gmail.com>
2026-03-14 13:38:17 -0700

    updated file_path

--- UI/home.py
+++ UI/home.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import re
+import os
 
 root = tk.Tk()
 root.title("Immigration System")
@@ -13,20 +14,27 @@ def load_schema(file):
 
     tables = {}
 
-    pattern = r"CREATE TABLE (\w+) \s*((.*?)\);"
-    matches = re.findall(pattern, schema, re.S)
+    pattern = r"CREATE TABLE (\w+)\s*\((.*?)\);"
+    matches = re.findall(pattern, schema, re.S | re.I)
 
-    for table, cols in matches:
-        columns = []
-        for col in cols.split(","):
+    for table, cols_block in matches:
+        cols = []
+        for col in cols_block.split(","):
             col_name = col.strip().split()[0]
-            columns.append(col)
+            if col_name.upper() not in ["PRIMARY", "FOREIGN", "CONSTRAINT"]:
+                cols.append(col_name)
 
-        tables[table] = columns
+        tables[table] = cols
 
     return tables
 
-entities = load_schema("schemas.sql")
+base_dir = os.path.dirname(__file__)
+schema_file = os.path.abspath(os.path.join(base_dir, "..", "Database", "schema.sql"))
+
+if not os.path.exists(schema_file):
+    raise FileNotFoundError(f"Cannot find schema at {schema_file}")
+
+entities = load_schema(schema_file)
 
 title = tk.Label(root, text="Immigration System", font=("Arial", 18))
 title.pack(pady=10)
