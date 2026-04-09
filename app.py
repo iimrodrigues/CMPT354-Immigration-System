2256c08985c5f39005f04e688edd7a0a6f529fac
iim <iim@sfu.ca>
2026-04-09 15:34:06 -0700

    add: visa query gui

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
 from db import get_connection
 
 app = Flask(__name__)
@@ -8,14 +8,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 @app.route("/select", methods=["POST"])
 def select():
+    print("SELECTED")
     entity = request.form.get("entity")
     action = request.form.get("action")
 
-    print(entity, action)
+    if entity == "Visa" and action == "Query":
+        return redirect(url_for("visa_query"))
 
     return f"You selected {entity} + {action}"
+
+## VISA ACTIONS
+@app.route("/visa/query", methods=["GET", "POST"])
+def visa_query():
+    return render_template("visa/query.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
